bf0203297553eb230706b9244cd640b230f1a64c
shivanshi-375 <iamshivanshigupta@gmail.com>
2021-11-14 01:42:24 -0500

    adding actions to start tile

--- code/src/entities/tiles/start/StartTile.java
+++ code/src/entities/tiles/start/StartTile.java
@@ -3,31 +3,33 @@ package entities.tiles.start;
 import entities.player.Player;
 import entities.tiles.tile.ActionStruct;
 import entities.tiles.tile.Tile;
+import entities.bank.Bank;
 
-//Author: Shivanshi
+// Author: Shivanshi
 
 public class StartTile extends Tile {
 
     @Override
-    public void onAction(ActionStruct info) {
+    public void onAction(Bank bank, Player player) {
+        bank.balance -= 200;
+        player.balance += 200;
     }
 
-    @Override
-    public boolean canAddPlayer(Player player) {
-        return false;
-    }
-
-    @Override
-    public void addPlayer(Player player) {
-    }
-
-    @Override
-    public boolean canRemovePlayer(Player player) {
-        return false;
-    }
-
-    @Override
-    public void removePlayer(Player player) {
-    }
+//    @Override
+//    public boolean canAddPlayer(Player player) { return true; }
+//
+//    @Override
+//    public void addPlayer(Player player) {
+//
+//    }
+//
+//    @Override
+//    public boolean canRemovePlayer(Player player) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void removePlayer(Player player) {
+//    }
     
 }
