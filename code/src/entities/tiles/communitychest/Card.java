e392b6b439a33b731c243a19fc52aece70f82487
DennisYako <dennisyakovlev40@gmail.com>
2021-11-15 14:16:47 -0500

    created abstract card type

---
+++ code/src/entities/tiles/communitychest/Card.java
@@ -0,0 +1,9 @@
+package entities.tiles.communitychest;
+
+import entities.tiles.tile.ActionStruct;
+
+public abstract class Card {
+    
+    public abstract void onAction(ActionStruct action);
+
+}
