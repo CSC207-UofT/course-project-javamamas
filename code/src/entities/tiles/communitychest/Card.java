2a3ad022492e54c05c1a90ea7d90af2af9def4a7
Dennis <dennis.yakovlev@yahoo.ca>
2021-12-08 22:07:47 -0500

    added final empty files

--- code/src/entities/tiles/communitychest/Card.java
+++
@@ -1,9 +0,0 @@
-package entities.tiles.communitychest;
-
-import entities.tiles.tile.ActionStruct;
-
-public abstract class Card {
-    
-    public abstract void onAction(ActionStruct action);
-
-}
